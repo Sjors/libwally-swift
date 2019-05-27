d5d1ff1ab6fa32234cf7d263e5584257018d354b
Sjors Provoost <sjors@sprovoost.nl>
2019-05-27 17:27:54 +0200

    Cocoa Touch Framework for LibWally

---
+++ LibWallySwift/LibWally.h
@@ -0,0 +1,19 @@
+//
+//  LibWally.h
+//  LibWally
+//
+//  Created by Sjors on 27/05/2019.
+//  Copyright © 2019 Blockchain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibWally.
+FOUNDATION_EXPORT double LibWallyVersionNumber;
+
+//! Project version string for LibWally.
+FOUNDATION_EXPORT const unsigned char LibWallyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibWally/PublicHeader.h>
+
+
