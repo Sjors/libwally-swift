eb7f93ca6a3cfefccfe3b7a82e59768199431dff
Sjors Provoost <sjors@sprovoost.nl>
2019-05-28 13:41:19 +0200

    Add Playground

---
+++ DemoPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+import LibWally
+
+print(BIP39Words.first!)
+
+let mnemonic = BIP39Mnemonic(["abandon", "abandon", "abandon", "abandon", "abandon", "abandon", "abandon", "abandon", "abandon", "abandon", "abandon", "about"])
+
+print(mnemonic!.words)
