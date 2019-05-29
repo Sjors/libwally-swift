37714bad45398c15c1c9a20f129a8fbf1e085dd3
Sjors Provoost <sjors@sprovoost.nl>
2019-05-29 11:19:43 +0200

    Add all libwally-core headers

--- LibWallySwift/LibWally.h
+++ LibWallySwift/LibWally.h
@@ -17,4 +17,11 @@ FOUNDATION_EXPORT const unsigned char LibWallyVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LibWally/PublicHeader.h>
 
+#import "wally_core.h"
+#import "wally_crypto.h"
+#import "wally_address.h"
+#import "wally_bip32.h"
+#import "wally_bip38.h"
 #import "wally_bip39.h"
+#import "wally_script.h"
+#import "wally_transaction.h"
