a93929c7eebce18734c60a21c1bab58870d1428b
Sjors Provoost <sjors@sprovoost.nl>
2019-06-18 14:06:47 -0400

    TxOutput: init with scriptPubKey and amount

--- LibWallySwift/Transaction.swift
+++ LibWallySwift/Transaction.swift
@@ -1,6 +1,6 @@
 //
 //  Transaction.swift
-//  Transaction 
+//  Transaction
 //
 //  Created by Sjors Provoost on 18/06/2019.
 //  Copyright © 2019 Blockchain. Distributed under the MIT software
@@ -8,9 +8,38 @@
 
 import Foundation
 
+typealias Satoshi = UInt64
+
+struct TxOutput {
+    let wally_tx_output: wally_tx_output
+    public var amount: Satoshi {
+        return self.wally_tx_output.satoshi
+    }
+    public let scriptPubKey: ScriptPubKey
+
+    init (_ scriptPubKey: ScriptPubKey, _ amount: Satoshi) {
+        self.scriptPubKey = scriptPubKey
+
+        var scriptpubkey_bytes = UnsafeMutablePointer<UInt8>.allocate(capacity: scriptPubKey.bytes.count)
+        let scriptpubkey_bytes_len = scriptPubKey.bytes.count
+
+        scriptPubKey.bytes.copyBytes(to: scriptpubkey_bytes, count: scriptpubkey_bytes_len)
+
+        var output: UnsafeMutablePointer<wally_tx_output>?
+        defer {
+            if let wally_tx_output = output {
+                wally_tx_output.deallocate()
+            }
+        }
+        precondition(wally_tx_output_init_alloc(amount, scriptpubkey_bytes, scriptpubkey_bytes_len, &output) == WALLY_OK)
+        precondition(output != nil)
+        self.wally_tx_output = output!.pointee
+    }
+}
+
 struct Transaction {
     let hash: Data
-    
+
     init? (_ description: String) {
         if description.count == 64 { // Transaction hash
             if let hash = Data(description) {
--- LibWallyTests/TransactionTests.swift
+++ LibWallyTests/TransactionTests.swift
@@ -1,6 +1,6 @@
 //
 //  TransactionTests.swift
-//  TransactionTests 
+//  TransactionTests
 //
 //  Created by Sjors Provoost on 18/06/2019.
 //  Copyright © 2019 Blockchain. Distributed under the MIT software
@@ -10,22 +10,29 @@ import XCTest
 @testable import LibWally
 
 class TransctionTests: XCTestCase {
-    
+
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
+
     func testFromHash() {
         let hash = ("0000000000000000000000000000000000000000000000000000000000000000")
         let tx = Transaction(hash)
         XCTAssertNotNil(tx)
         XCTAssertEqual(tx?.hash.hexString, hash)
-        
+
         XCTAssertNil(Transaction("00")) // Wrong length
     }
-    
+
+    func testOutput() {
+        let scriptPubKey = ScriptPubKey("76a914bef5a2f9a56a94aab12459f72ad9cf8cf19c7bbe88ac")!
+        let output = TxOutput(scriptPubKey, 1000)
+        XCTAssertNotNil(output)
+        XCTAssertEqual(output.amount, 1000)
+        XCTAssertEqual(output.scriptPubKey, scriptPubKey)
+    }
 }
