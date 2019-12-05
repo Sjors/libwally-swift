249fe1e4ec4fb78918baf8f015b2bc83cc98f243
Sjors Provoost <sjors@sprovoost.nl>
2019-12-05 14:54:49 +0100

    Create multisig address

--- LibWallyTests/ScriptTests.swift
+++ LibWallyTests/ScriptTests.swift
@@ -71,7 +71,12 @@ class ScriptTests: XCTestCase {
     func testMultisig() {
         let pubKey1 = PubKey("03501e454bf00751f24b1b489aa925215d66af2234e3891c3b21a52bedb3cd711c")! // [3442193e/0'/1]
         let pubKey2 = PubKey("022e3d55c64908832291348d1faa74bff4ae1047e9777a28b26b064e410a554737")! // [bd16bee5/0'/1]
-        var multisig = ScriptPubKey(multisig: [pubKey1, pubKey2], threshold: 2)
+        let multisig = ScriptPubKey(multisig: [pubKey1, pubKey2], threshold: 2)
         XCTAssertEqual(multisig.type, .multiSig)
+        XCTAssertEqual(multisig.bytes.hexString, "5221022e3d55c64908832291348d1faa74bff4ae1047e9777a28b26b064e410a5547372103501e454bf00751f24b1b489aa925215d66af2234e3891c3b21a52bedb3cd711c52ae")
+        XCTAssertEqual(multisig.witnessProgram.hexString, "0020ce8c526b7a6c9491ed33861f4492299c86ffa8567a75286535f317ddede3062a")
+
+        let address = Address(multisig, .mainnet)
+        XCTAssertEqual(address?.address, "bc1qe6x9y6m6dj2frmfnsc05fy3fnjr0l2zk0f6jsef47vtamm0rqc4qnfnxm0")
     }
 }
