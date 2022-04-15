0925fc89354d092659a95a7d6f0c4e88912756e9
Jurvis Tan <5944973+jurvis@users.noreply.github.com>
2022-04-14 19:53:17 -0700

    Draft Package.swift
    Try with LibwallySwift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let tag = "0.0.7"
+let checksum = "248516a8014f910ab786eb31f48cb2fced2fb527d3378520cf1e13b1324d38b9"
+let url = "https://github.com/jurvis/libwally-swift/releases/download/\(tag)/LibwallySwift.xcframework.zip"
+
+let package = Package(
+    name: "LibWallySwift",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "LibwallySwift",
+            targets: ["LibwallySwift"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "LibwallySwift",
+            url: url,
+            checksum: checksum
+        )
+    ]
+)
