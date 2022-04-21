799e8e74ada7a751a926fc8ed7ded906a2a4cfe0
Jurvis Tan <5944973+jurvis@users.noreply.github.com>
2022-04-20 21:30:28 -0700

    Move autogen aux files to build-libwally

--- build-libwally.sh
+++ build-libwally.sh
@@ -5,9 +5,18 @@ if [[ -f "${HOME}/.bash_profile" ]]; then
   source "${HOME}/.bash_profile"
 fi
 
-export PATH="$PATH:/usr/bin/python3"
-export PYTHONPATH="${PYTHONPATH}:/usr/bin/python3"
+export PATH=$PATH:/opt/homebrew/bin/
 export PYTHON="/usr/bin/python3"
+
+cd CLibWally/libwally-core
+pushd src/secp256k1
+  # Latest commit used in Bitcoin Core:
+  # https://github.com/bitcoin/bitcoin/commits/master/src/secp256k1
+  git checkout 8746600eec5e7fcd35dabd480839a3a4bdfee87b || exit 1
+popd
+sh ./tools/autogen.sh
+
+cd $PROJECT_DIR
 
 if [[ ${ACTION:-build} = "build" ]]; then
   if [[ $PLATFORM_NAME = "macosx" ]]; then
@@ -38,6 +47,7 @@ if [[ ${ACTION:-build} = "build" ]]; then
     pushd "CLibWally/libwally-core"
       export CFLAGS="-O3 ${ARCHES[@]} -fembed-bitcode -mios-version-min=11.0 -isysroot `xcrun -sdk ${PLATFORM_NAME} --show-sdk-path`"
       export CXXFLAGS="-O3 ${ARCHES[@]} -fembed-bitcode -mios-version-min=11.0 -isysroot `xcrun -sdk ${PLATFORM_NAME} --show-sdk-path`"
+
       ./configure --disable-shared --host="${TARGET_ARCH}-apple-${TARGET_OS}" --enable-static --disable-elements --enable-standard-secp
       make
     popd
