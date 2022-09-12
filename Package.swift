dc879551f18a22d3451f1c3891f9cebb03038ed3
Jurvis Tan <hello@jurvis.co>
2022-09-12 14:24:37 -0700

    Update checksum
    Update checksum

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let tag = "0.0.7"
-let checksum = "62aca7cefdf59cfe96d152e12cfd56387e9960e99523527ff478508f70ac25da"
+let checksum = "f2d8bbf4b6cf3cf4f4063b6a15a84648d78a3c7214f8836182b9f83ed8a2a595"
 let url = "https://github.com/jurvis/libwally-swift/releases/download/\(tag)/LibWally.xcframework.zip"
 
 let package = Package(
